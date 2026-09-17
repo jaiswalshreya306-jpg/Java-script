379753ce86ca2ae03f095282c52474108c907dbb
jaiswalshreya306-jpg <jaiswalshreya306@gmail.com>
2026-09-17 22:43:45 +0530

    keep going

---
+++ learning-2/lecture8part2.js
@@ -0,0 +1,10 @@
+const user = {
+    name: "rohit",
+    age: 20,
+    emailId:"jaiswalshreay@gamil.com",
+    amount: 3400
+}
+console.log(user["name"]);
+console.log(user.age);
+console.log(user["age"]);
+console.log(user["home address"]);
