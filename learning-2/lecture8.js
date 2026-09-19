41f40cdd9d89afa1b4e58ce4dbbd22fbacb9eef5
jaiswalshreya306-jpg <jaiswalshreya306@gmail.com>
2026-09-19 19:33:57 +0530

    learn about the obejct

---
+++ learning-2/lecture8.js
@@ -0,0 +1,57 @@
+//const user ={
+   // name: "rohit",
+  //  age: 20,
+  //  emailID:"jaiswalshreya.com",
+//    amount: 3400,
+//}
+//const user2 = user;
+//user2.age = 90;
+
+//console.log(user);
+//important
+
+//console.log(Object.keys(user));
+
+//console.log(object.values(user));
+//console.log(object.entries(user));
+//for(let keys in user){
+//    console.log(keys,user[keys]);
+//}
+
+//for(let keys of Object.values()){
+  //  console.log(keys);
+//}
+//for(let of Object.entries(user)){
+
+  //console.log(values);
+//}\
+//for(let [keys,values] of Object.entries(user)){
+//    console.log(keys,values);
+//}
+
+//const user ={
+    //name: "rohit",
+    //age: 20,
+    //emailID:"jaiswalshreya.com",
+    //amount: 3400,
+    //greeting : function(){
+    //    console.log("strike is coming on 17 october");
+   // }
+  //  }
+//user.greeting();
+
+
+const user ={
+    name: "rohit",
+    age: 20,
+    emailID:"jaiswalshreya.com",
+    amount: 3400,
+    address: {
+        city:"kotdwar",
+        state:"uttarkhand"
+    }
+}
+console.log(user);
+
+
+
