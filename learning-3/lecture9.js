2114196ef1d17df4d357caff2a8d68f2ee28cd95
jaiswalshreya306-jpg <jaiswalshreya306@gmail.com>
2026-09-21 22:53:16 +0530

    keep going

---
+++ learning-3/lecture9.js
@@ -0,0 +1,31 @@
+// function
+
+//function greeting(){
+ //   console.log("Hello coder army, stike is coming on 8 october");
+//greeting();
+//}
+
+
+//function addNumber(...num){
+     //let sum=0;
+     //for(let n of sum){
+        //sum+=n;
+
+    // }
+  //   comsole.log(sum);
+
+//}
+
+//addNumber(5,8);
+//addNumber(7,6,7,8);
+//addNumber(7,8,9,0,8,87,65,67); 
+
+
+//arrow function
+
+
+const addNumber=(num1,num2)=>{
+    return num1+num2;
+
+}
+console.log(addNumber(8,9));
