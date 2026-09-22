040aaf6773355e4d7dbc3781b028fe53e60f3e81
jaiswalshreya306-jpg <jaiswalshreya306@gmail.com>
2026-09-22 22:04:08 +0530

    Today a reader tomarrow a leader

---
+++ learning-4/lecture9.js
@@ -0,0 +1,30 @@
+//(function greeting(){
+ //   console.log("helloji");
+
+//})();
+
+
+//function greet(){
+ //   console.log("hello ji kaise ho")
+
+//}
+//function meet(){
+  //  console.log("i am going to met someone");
+
+//}
+//meet();
+//greet();
+
+function zomatoOrderPlaced(){
+    console.log("we have started preparing your food");
+
+}
+function payment(amount){
+    console.log(`${amount} payment has initilized`)
+    console.log("payment is received");
+    zomatoOrderPlaced();
+
+}
+//gst: government
+//rider ko kkitna pament  dena 
+// company ko kitna dena h
